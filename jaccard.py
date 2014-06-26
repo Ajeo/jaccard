6279b3e47d563b1dfa6b98243d34fc9ab2b60b7f
Jorge Soto <jorge.o.soto@gmail.com>
2014-06-25 20:51:53 -0400

    Add topic label to top terms using jaccard index

--- jaccard.py
+++ jaccard.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-import os
+from os import listdir
+from os.path import isfile, join
 import csv
-import codecs
 
 def csv_writer(data, path):
     with open(path, "wb") as csv_file:
@@ -24,27 +24,38 @@ def jaccard(list1, list2):
 
 if ( __name__ == "__main__"):
     topics = readTopics()
-    all_top_terms = []
-    with open('./input/acm_trans_design_autom_electr_syst-top-terms.csv', 'rb') as f:
-        reader = csv.reader(f)
-        reader.next()
-        for row in reader:
-            all_top_terms.append(row[1].strip().split(' '))
 
-    output = [['Topic', 'Top Terms']]
-    names = {}
-    for top_terms in all_top_terms:
-        max = 0.0
-        name = ''
-        for topic in topics:
-            value = jaccard(top_terms, topic.values()[0])
-            if max >= value:
-                name = topic.keys()[0]
-                if name in names.keys():
-                    names[name] = names[name] + 1
-                    name = name + '-' + str(names[name])
-                else:
-                    names[name] = 0
-        output.append([name, ' '.join(top_terms)])
-    print output
-    csv_writer(output, './output/acm_trans_design_autom_electr_syst-top-terms.csv')
+    root_dir = "./input"
+    files = [ f for f in listdir(root_dir) if isfile(join(root_dir,f)) ]
+
+    if files[0] == '.DS_Store':
+        files.pop(0)
+
+    for fi in files:
+        print fi
+
+        all_top_terms = []
+        with open('./input/' + fi, 'rb') as f:
+            reader = csv.reader(f)
+            reader.next()
+            for row in reader:
+                all_top_terms.append(row[1].strip().split(' '))
+
+        output = [['Topic', 'Top Terms']]
+        names = {}
+
+        for top_terms in all_top_terms:
+            max = 0.0
+            name = ''
+            for topic in topics:
+                value = jaccard(top_terms, topic.values()[0])
+                if max >= value:
+                    name = topic.keys()[0]
+                    if name in names.keys():
+                        names[name] = names[name] + 1
+                        name = name + '-' + str(names[name])
+                    else:
+                        names[name] = 0
+            output.append([name, ' '.join(top_terms)])
+
+        csv_writer(output, './output/' + fi)
